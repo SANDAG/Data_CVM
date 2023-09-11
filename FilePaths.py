94fcd8bcec05e6ee1c7165dae7f57160fc53f3b5
John Gliebe <johnpgliebe@users.noreply.github.com>
2023-09-11 12:44:06 -0400

    Add household deliveries notebook and file paths
    This version is for getting total weighted households by Food, Package, and Service stops.

---
+++ FilePaths.py
@@ -0,0 +1,9 @@
+# Data file paths
+
+root_dir = r"C:\Users\jgliebe\OneDrive - Cambridge Systematics\Documents - PROJ SANDAG Commercial Vehicle & Heavy Truck Model Update\_Shared_CSTeam"
+
+raw_data_dir = r"Task03_DataID_Review"
+
+proc_data_dir = r"Task04_DataProcessing\Data\proc data\v11"
+
+lookup_dir = r"Task04_DataProcessing\Data\Lookups"
